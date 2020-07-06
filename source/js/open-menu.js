f9f7fc69d61f96580c19aa33a2dca463707646af
Lada-Ovchnnikova <lada.ovchinnikova@mail.ru>
2020-07-06 23:05:20 +0300

    initial commit

---
+++ source/js/open-menu.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var navMain = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navMain.classList.remove('page-header--nojs');
+navMain.classList.remove('page-header--opened');
+navMain.classList.add('page-header--closed');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('page-header--closed')) {
+    navMain.classList.remove('page-header--closed');
+    navMain.classList.add('page-header--opened');
+  } else {
+    navMain.classList.add('page-header--closed');
+    navMain.classList.remove('page-header--opened');
+  }
+});
